2c2a52376354d7028144d3ea31c08085d314ceb8
Dennis Onder <webdev.dennisonder@gmail.com>
2018-09-22 16:54:19 +0200

    Finished basic modal structure and styles - TODO: Implement the ability to send new issues from the modal, and integrate it into the dashboard

--- client/src/components/modal/Modal.js
+++ client/src/components/modal/Modal.js
@@ -1,6 +1,33 @@
 import React, { Component } from 'react'
 
 class Modal extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+        user: '',
+        link: ''
+    }
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onSubmit() {
+    console.log({user: this.state.user, link: this.state.link});
+    this.setState({
+      user: '',
+      link: ''
+    })
+    document.getElementById('userInput').value = '';
+    document.getElementById('urlInput').value = '';
+  }
+
+  onChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
   render() {
     return (
       <div className="Modal">
@@ -10,11 +37,11 @@ class Modal extends Component {
             <i className="fas fa-times-circle" id="closeBtn"></i>
           </div>
             <div className="modalInputs">
-              <input type="text" placeholder="GitHub Username:" id="userInput"/>
+              <input type="text" placeholder="GitHub Username:" id="userInput" name="user" ref={this.userInput} value={this.state.user} onChange={this.onChange} />
               <br/>
-              <input type="text" placeholder="Repository URL:" id="urlInput"/>
+              <input type="text" placeholder="Repository URL:" id="urlInput" name="link" ref={this.linkInput} value={this.state.link} onChange={this.onChange} />
               <br/>
-              <button id="submitBtn">Submit!</button>
+              <button id="submitBtn" onClick={this.onSubmit}>Submit!</button>
               <br/>
             </div>
         </div>
