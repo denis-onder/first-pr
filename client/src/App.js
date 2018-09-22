5593d918ca25b5526647abf68b46673ce066f8d9
Dennis Onder <webdev.dennisonder@gmail.com>
2018-09-22 18:31:44 +0200

    Deleted obsolete testing routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Test from './Test';
 import Dashboard from './components/dashboard/Dashboard';
 import './App.css';
-import Modal from './components/modal/Modal';
 
 class App extends Component {
 
@@ -12,8 +10,6 @@ class App extends Component {
       <Router>
         <div>
           <Route exact path="/" component={Dashboard} />
-          <Route exact path="/test" component={Test} />
-          <Route exact path="/modal" component={Modal} />
         </div>
       </Router>
     );
