e390255da87f11db14d1b2396bffcdf472ce6c9f
Dennis Onder <webdev.dennisonder@gmail.com>
2018-09-22 16:40:35 +0200

    Added modal styles

--- client/src/components/modal/Modal.js
+++ client/src/components/modal/Modal.js
@@ -9,12 +9,13 @@ class Modal extends Component {
             <h3>Add a new issue!</h3>
             <i className="fas fa-times-circle" id="closeBtn"></i>
           </div>
-            <div className="inputGroup">
-              <input type="text"/>
+            <div className="modalInputs">
+              <input type="text" placeholder="GitHub Username:" id="userInput"/>
               <br/>
-              <input type="text"/>
+              <input type="text" placeholder="Repository URL:" id="urlInput"/>
               <br/>
               <button id="submitBtn">Submit!</button>
+              <br/>
             </div>
         </div>
       </div>
