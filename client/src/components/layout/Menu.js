791c69a7e63ec77f3073bd3014d5c9f427af4e78
Dennis Onder <webdev.dennisonder@gmail.com>
2018-09-24 02:57:44 +0200

    Added styles to the menu links

--- client/src/components/layout/Menu.js
+++ client/src/components/layout/Menu.js
@@ -14,25 +14,33 @@ class Menu extends Component {
       <div className="Menu" id="menu">
         <i className="fas fa-arrow-right fa-2x" id="closeMenuBtn" onClick={this.closeMenu}></i>
         <ul>
-          <li>
+          <li id="menuTitle">
             <p>Learning Resources:</p>
           </li>
-          <li>
-            <i className="fab fa-reddit"></i>
-            <a href="https://www.reddit.com/r/learnprogramming/" target="_blank" rel="noopener noreferrer">Learn Programming</a>
-          </li>
-          <li>
-            <i className="fab fa-reddit"></i>
-            <a href="https://www.reddit.com/r/learnjavascript/" target="_blank" rel="noopener noreferrer">Learn JavaScript</a>
-          </li>
-          <li>
-            <i className="fab fa-reddit"></i>
-            <a href="https://www.reddit.com/r/learnpython/" target="_blank" rel="noopener noreferrer">Learn Python</a>
-          </li>
-          <li>
-            <i className="fab fa-reddit"></i>
-            <a href="https://www.reddit.com/r/learnruby/" target="_blank" rel="noopener noreferrer">Learn Ruby</a>
-          </li>
+          <a href="https://www.reddit.com/r/learnprogramming/" target="_blank" rel="noopener noreferrer">
+            <li className="listItem">
+              <i className="fab fa-reddit"></i>
+              <p>Learn Programming</p>
+            </li>
+          </a>
+          <a href="https://www.reddit.com/r/learnjavascript/" target="_blank" rel="noopener noreferrer">
+            <li className="listItem">
+              <i className="fab fa-reddit"></i>
+              <p>Learn Javascript</p>
+            </li>
+          </a>
+          <a href="https://www.reddit.com/r/learnpython/" target="_blank" rel="noopener noreferrer">
+            <li className="listItem">
+              <i className="fab fa-reddit"></i>
+              <p>Learn Python</p>
+            </li>
+          </a>
+          <a href="https://www.reddit.com/r/learnruby/" target="_blank" rel="noopener noreferrer">
+            <li className="listItem">
+              <i className="fab fa-reddit"></i>
+              <p>Learn Ruby</p>
+            </li>
+          </a>
         </ul>
       </div>
     )
