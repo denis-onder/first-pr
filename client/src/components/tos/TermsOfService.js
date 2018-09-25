e30e3ebab5ea4f9fb9e25871a2569672e0c1c0b3
Dennis Onder <webdev.dennisonder@gmail.com>
2018-09-25 02:00:36 +0200

    Added basic ToS component  structure

--- client/src/components/tos/TermsOfService.js
+++ client/src/components/tos/TermsOfService.js
@@ -1,10 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Sidebar from '../layout/Sidebar';
 
 export default class TermsOfService extends Component {
+
+  componentDidMount() {
+    document.getElementById('title').style.display = 'block';
+  }
+
   render() {
     return (
-      <div>
-        <h1>Hello</h1>
+      <div className="Dashboard">
+        <Sidebar />
+        <h1 id="title">Terms of Service:</h1>
       </div>
     )
   }
