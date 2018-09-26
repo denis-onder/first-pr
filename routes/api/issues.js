69bcbb837bee1018fb1a2b77da3febf6b13db1f8
Dennis Onder <webdev.dennisonder@gmail.com>
2018-09-27 01:45:55 +0200

    Added back-end validation

--- routes/api/issues.js
+++ routes/api/issues.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const Issue = require('../../models/Issue');
+const Validator = require('validator');
 
 // Get all issues
 router.get('/', (req, res) => {
@@ -12,14 +13,34 @@ router.get('/', (req, res) => {
 
 // Post a new issue
 router.post('/', (req, res) => {
-  const newIssue = new Issue({
-    user: req.body.user,
-    link: req.body.link,
-    description: req.body.description
-  });
-  newIssue.save()
-    .then(post => res.json(post))
-    .catch(err => res.json(err));
+  let errors = {}
+  if (Validator.isEmpty(req.body.user)) {
+    errors.user = 'User Field must be filled out.';
+  }
+  if (Validator.isEmpty(req.body.description)) {
+    errors.description = 'Description Field must be filled out.';
+  }
+  if (Validator.isEmpty(req.body.link)) {
+    errors.link = 'URL Field must be filled out.';
+  }
+  if (!Validator.isURL(req.body.link)) {
+    errors.link = 'The Link must be a valid URL.';
+  }
+  if (!req.body.link.includes('github')) {
+    errors.link = 'The Link must be a valid GitHub URL.';
+  }
+  if (errors.user || errors.link || errors.description) {
+    res.status(400).json(errors);
+  } else {
+    const newIssue = new Issue({
+      user: req.body.user,
+      link: req.body.link,
+      description: req.body.description
+    });
+    newIssue.save()
+      .then(post => res.json(post))
+      .catch(err => res.json(err));
+  }
 });
 
 module.exports = router;
