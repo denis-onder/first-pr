2b0eedfbdeb0fe3e645027296196a718a818c672
Dennis Onder <webdev.dennisonder@gmail.com>
2018-10-13 13:58:33 +0200

    Postbuild Scripts

--- express.js
+++ express.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
 const port = process.env.PORT || 8000;
 const issues = require('./routes/api/issues');
+const path = require('path');
 
 // BodyParser
 app.use(bodyParser.urlencoded({extended:false}));
@@ -26,6 +27,13 @@ app.get('/test', (req, res) => {
 
 // Router
 app.use('/api/issues', issues);
+
+if(process.env.NODE_ENV === 'production') {
+ app.use(express.static('client/build'));
+ app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+ });
+}
 
 // Server
 app.listen(port, () => console.log(`Server is running on port: ${port}.`));
